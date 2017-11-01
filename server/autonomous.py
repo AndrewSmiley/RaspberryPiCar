c2f7fe850372188cace1449d899c75b38db0ed49
Andrew Smiley <andrew.smiley@ge.com>
2017-11-01 12:39:50 -0400

    first autonomous upload

--- server/autonomous.py
+++ server/autonomous.py
@@ -35,13 +35,13 @@ def do_forward(_time_delta, start_time):
     :return:
     '''
 
-    while datetime.timedelta(start_time-datetime.datetime.now()).seconds < _time_delta:
+    while (start_time-datetime.datetime.now()).seconds < _time_delta:
         motor.backward()
 
 
 import time
 start_time = datetime.datetime.now()
-print datetime.timedelta(start_time-datetime.datetime.now()).seconds
+print (start_time-datetime.datetime.now()).seconds
 time.sleep(10)
 do_forward(1,start_time )
 
